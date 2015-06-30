c3641db3bd5485fe8f0ee537fa56ed9c9ac9e4d1
student <AJ@AJs-MacBook-Pro.local>
2015-06-30 08:37:10 -0500

    Initial Commit

---
+++ Chainlink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chainlink
+//
+//  Created by AJ Priola on 6/30/15.
+//  Copyright © 2015 AJ Priola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
